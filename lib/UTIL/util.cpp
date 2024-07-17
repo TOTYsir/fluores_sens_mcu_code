8242bb9ef1c96a99c213c1b62ad0b62ea677d37b
Yue Wang <81452197+TOTYsir@users.noreply.github.com>
2024-07-17 21:43:59 +0100

    waiting for the actual test on ESP32

--- lib/UTIL/util.cpp
+++ lib/UTIL/util.cpp
@@ -3,12 +3,12 @@
 
 float get_norm(float values0[], float values1[]) {
     // CHANNEL 0
-    float on_avg_ch0 = calculate_average(values0, 1, SAMPLES_PER_ON_CYCLE + 1); // start index = 1
-    float off_avg_ch0 = calculate_average(values0, SAMPLES_PER_ON_CYCLE + 1, SAMPLES_PER_OFF_CYCLE);
+    float on_avg_ch0 = calculate_average(values0, 0, SAMPLES_PER_ON_CYCLE); // start index = 1
+    float off_avg_ch0 = calculate_average(values0, SAMPLES_PER_ON_CYCLE, SAMPLES_PER_OFF_CYCLE);
 
     // CHANNEL 1
-    float on_avg_ch1 = calculate_average(values1, 1, SAMPLES_PER_ON_CYCLE + 1);
-    float off_avg_ch1 = calculate_average(values1, SAMPLES_PER_ON_CYCLE + 1, SAMPLES_PER_OFF_CYCLE);
+    float on_avg_ch1 = calculate_average(values1, 0, SAMPLES_PER_ON_CYCLE);
+    float off_avg_ch1 = calculate_average(values1, SAMPLES_PER_ON_CYCLE, SAMPLES_PER_OFF_CYCLE);
 
     return (on_avg_ch0 - off_avg_ch0) / (on_avg_ch1 - off_avg_ch1);
 }
